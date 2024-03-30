9f1b1a547bdca1e355ffc5c1796f8115b9144848
Dottyyyyy <Orekihoutarou126@gmail.com>
2024-03-30 19:18:33 +0800

    Register / Frontend Setup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,10 @@
-const upload = require('../utils/multer');
-const router = express.Router();
 const express = require('express');
+const router = express.Router();
+const upload = require('../utils/multer');
 
 const userController = require('../controllers/UserController')
 
 router.post('/register', upload.single('image'), userController.register)
-
-
 
 
 
