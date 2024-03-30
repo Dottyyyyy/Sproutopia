e7d4801b69ab3de6c376dc5db9b8825a29b1cef5
Mary Jane C. Raras <maryjane.raras@tup.edu.ph>
2024-03-31 01:34:37 +0800

    Product CRUD

--- frontend/Navigators/AdminNavigator.js
+++ frontend/Navigators/AdminNavigator.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 // import Orders from "../Screens/Admin/Orders"
 import Products from "../Screens/Admin/Products"
 import ProductForm from "../Screens/Admin/ProductForm"
-import Category from "../Screens/Admin/Categories";
+import Categories from "../Screens/Admin/Categories";
 
 const Stack = createStackNavigator();
 
@@ -18,7 +18,7 @@ const AdminNavigator = () => {
                     title: "Products"
                 }}
             /> }
-      <Stack.Screen name="Categories" component={Categories} />
+      <Stack.Screen name="Categories" component={categories} />
       {/* <Stack.Screen name="Orders" component={Orders} />
             <Stack.Screen name="ProductForm" component={ProductForm} /> */}
     </Stack.Navigator>
