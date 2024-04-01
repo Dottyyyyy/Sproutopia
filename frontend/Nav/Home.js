ea858f539a6df4966a4e448fb3200ad1b2c2dba0
Mary Jane C. Raras <maryjane.raras@tup.edu.ph>
2024-04-01 21:46:18 +0800

    error

--- frontend/Nav/Home.js
+++ frontend/Nav/Home.js
@@ -1,34 +1,32 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'
+import { View, Text, StyleSheet } from 'react-native';
 
 import ProductContainer from "../Screens/Product/ProductContainer";
-import SingleProduct from "../Screens/Product/SingleProduct";
-
+import SingleProduct from '../Screens/Product/SingleProduct';
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-    <View style={styles.container}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Product Container"
-          component={ProductContainer}
-          options={{
-            headerShown: false,
-          }}
-        />
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Home'
+        component={ProductContainer}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name='Product Detail'
+        component={SingleProduct}
+        options={({ route }) => ({ headerShown: route.params && route.params.showHeader ? route.params.showHeader : false })}
+      />
+    </Stack.Navigator>
+  )
+}
 
-        <Stack.Screen
-          name="Product Detail"
-          component={SingleProduct}
-          // options={{
-          //     headerShown: false,
-          // }}
-        />
-      </Stack.Navigator>
-    </View>
-  );
+export default function Home() {
+  return <MyStack />;
 }
 const styles = StyleSheet.create({
   container: {
@@ -44,4 +42,4 @@ const styles = StyleSheet.create({
   },
   // Add more styles as needed
 });
-export default Home;
+
