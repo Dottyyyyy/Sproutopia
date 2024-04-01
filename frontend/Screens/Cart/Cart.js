a83726fc1edea6dfd0e64f9286dd3b09edc42855
Mary Jane C. Raras <maryjane.raras@tup.edu.ph>
2024-04-01 17:01:27 +0800

    error

--- frontend/Screens/Cart/Cart.js
+++ frontend/Screens/Cart/Cart.js
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useNavigation } from '@react-navigation/native';
 import Icon from "react-native-vector-icons/FontAwesome";
 import { SwipeListView } from 'react-native-swipe-list-view';
-import { removeFromCart, clearCart } from '../../Redux/Actions/CartActions'
+import { removeFromCart, clearCart } from '../../Redux/Actions/cartActions'
 // var { height, width } = Dimensions.get("window");
 import EasyButton from "../../Shared/StyledComponents/EasyButton"
 import AuthGlobal from "../../Context/Store/AuthGlobal"
