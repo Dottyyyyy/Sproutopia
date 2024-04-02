e2bece68b3d95295711e6b17dddc401b9c0b6971
Mary Jane C. Raras <maryjane.raras@tup.edu.ph>
2024-04-02 18:09:19 +0800

    naiiyak nako

--- frontend/Redux/store.js
+++ frontend/Redux/store.js
@@ -1,26 +1,27 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'; // Import Redux Thunk middleware
-// import { userReducer } from './Reducers/userReducers';
-// import { getUser, getToken } from '../utils/user';
+import thunk from 'redux-thunk'; 
+import { userReducer } from './Reducers/userReducers';
+import { getUser, getToken } from '../utils/user';
 import cartItems from './Reducers/CartReducers';
 
 const reducers = combineReducers({
     user: userReducer,
     cartItems: cartItems,
 })
-// let initialState = {
+let initialState = {
 
-//     user: {
-//         userInfo: getUser(),
-//         token: getToken()
-//     },
+    user: {
+        userInfo: getUser(),
+        token: getToken()
+    },
 
-// }
+}
 
 const store = createStore(
     reducers,
-    // initialState,
-    applyMiddleware(thunk)
+    initialState,
 )
+
+const enhancedCreateStore = applyMiddleware(thunk)(createStore);
 
 export default store;
